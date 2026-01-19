3775b94c5592e757d843b81c33382f8fd8de7690
Anshad ck <anshadck1020@gmail.com>
2026-01-19 11:35:59 +0530

    Edited Power_Aware_Interrupt_System _Mini Project

---
+++ Project/Power_Aware_Interrupt_System _Mini Project/Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_gpio_driver.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_rcc_driver.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_usart_driver.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_low_power.h
+C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx.h:
+C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_gpio_driver.h:
+C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx.h:
+C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_rcc_driver.h:
+C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_usart_driver.h:
+C:/Users/HP/Documents/C\ Programming/STM32/Project/Power_Aware_Interrupt_System\ _Mini\ Project/Driver/Inc/stm32f446xx_low_power.h:
