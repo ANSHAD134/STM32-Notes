d189c02228a0706fe8cf70738d902cd6e3504f3c
Anshad ck <anshadck1020@gmail.com>
2026-02-09 21:32:02 +0530

    Edited Assignment

--- Assignment/Two_LED_Program/Src/Gpio_Led.c
+++ Assignment/Two_LED_Program/Src/Gpio_Led.c
@@ -19,3 +19,39 @@ void delay2(void){
 	for(uint32_t i = 0; i<500000; i++);
 }
 
+int main(void){
+
+	GPIO_Handle_t GpioLed;
+
+	GpioLed.pGPIOx = GPIOA;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	GPIO_Handle_t GpioButton;
+
+	// B1 Button (PC13)
+	GpioButton.pGPIOx = GPIOC;
+	GpioButton.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GpioButton.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GpioButton.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioButton.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOC, ENABLE);
+	GPIO_Init(&GpioButton);
+
+	while(1){
+
+		if(GPIO_ReadFromInputPin(GPIOC, GPIO_PIN_NO_13) == BUTTON_PRESSED)
+		{
+			GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+			delay();
+		}
+	}
+
+}
