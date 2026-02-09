8568ea21850d275918cb379b94cd44903b93832b
Anshad ck <anshadck1020@gmail.com>
2026-02-09 21:29:57 +0530

    Edited Assignment

--- Assignment/Two_LED_Program/Src/Gpio_Led.c
+++ Assignment/Two_LED_Program/Src/Gpio_Led.c
@@ -7,3 +7,15 @@
 
 #include "stm32f446xx.h"
 #include "stm32f446xx_gpio_driver.h"
+
+#define BUTTON_PRESSED 			0
+volatile uint8_t BUTTON_PRESS = 1;
+
+void delay1(void){
+	for(uint32_t i = 0; i<500000; i++);
+}
+
+void delay2(void){
+	for(uint32_t i = 0; i<500000; i++);
+}
+
