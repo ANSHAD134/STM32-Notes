e790b6bb35440264146bf71ddf165338533f3c51
Anshad ck <anshadck1020@gmail.com>
2026-03-13 21:10:01 +0530

    Edited DriverDev

--- Assignment/Two_LED_Program/Src/Gpio_Led.c
+++ Assignment/Two_LED_Program/Src/Gpio_Led.c
@@ -23,9 +23,6 @@ int main(void){
 	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
 	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
-	GPIO_PeriClockControl(GPIOA, ENABLE);
-	GPIO_Init(&GpioLed);
-
 	GpioLed.pGPIOx = GPIOA;
 	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_7;
 	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
