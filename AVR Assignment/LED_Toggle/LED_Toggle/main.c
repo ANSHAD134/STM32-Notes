5733d49a8b8e77ee9e80042390e31e27fdefdde3
Anshad ck <anshadck1020@gmail.com>
2026-05-01 20:22:48 +0530

    Added LED_Toggle Program

---
+++ AVR Assignment/LED_Toggle/LED_Toggle/main.c
@@ -0,0 +1,26 @@
+/*
+ * LED_Toggle.c
+ *
+ * Created: 01-05-2026 7.39.02 PM
+ * Author : HP
+ */ 
+
+#define F_CPU 16000000U
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRD |= (1 << DDD5);
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTD |= (1 << PORTD5);				// Set PORTB5 for LED ON
+		_delay_ms(3000);					// 3 seconds Delay
+		PORTD &=~(1 << PORTD5);				// Clear PORTB5 for LED OFF
+		_delay_ms(5000);					// 5 Seconds Delay  
+    }
+}
+
