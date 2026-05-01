d4e1a28a990693d76e2f158931b81589163d815a
Anshad ck <anshadck1020@gmail.com>
2026-05-01 21:26:57 +0530

    Added LED_Toggle Program

--- AVR Assignment/Push-button_Toggle_LEDS/Push-button_Toggle_LEDS/main.c
+++ AVR Assignment/Push-button_Toggle_LEDS/Push-button_Toggle_LEDS/main.c
@@ -5,14 +5,46 @@
  * Author : HP
  */ 
 
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
+	DDRD |= (1 << DDD5);			// Green LED
+	DDRD |= (1 << DDD3);			// Red LED
+	DDRD &= ~(1 << DDD1);			// Push-Button
+	PORTD |= (1 << PORTD1);			// Pull-up
+	
+	uint8_t state = 1;
+	
+	PORTD &=~ (1 << PORTD5);		// Set initial GREEN LED is OFF 
+	PORTD |= (1 << PORTD3);			// Set initial RED LED is ON
     /* Replace with your application code */
     while (1) 
     {
-    }
+		if(!(PIND & (1 << PIND1)))
+		{
+			_delay_ms(50);							// Debounce Delay
+			
+			if (!(PIND & (1 << PIND1)))
+			{
+				state = !state;						// Toggle LED state
+				
+				if(state)
+				{
+					PORTD |= (1 << PORTD3);			// Set PORTD3 for RED LED ON 
+					PORTD &= ~(1 << PORTD5);		// Clear PORTD5 for GREEN LED OFF
+				}
+				else
+				{
+					PORTD &= ~(1 << PORTD3);		// Clear PORTD3 for RED LED OFF
+					PORTD |= (1 << PORTD5);			// Set PORTD5 for GREEN LED ON
+				}
+				while (!(PIND & (1 << PIND1)));		// waits until Release the Button
+			}
+		}
+	}
 }
-
