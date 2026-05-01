b0c3f634de1696938d897deef17e381b75c023c3
Anshad ck <anshadck1020@gmail.com>
2026-05-01 17:38:44 +0530

    Added Program-8

---
+++ C Assignment/Program-8.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+void ConvertBase(int num, int base, char result[])
+{
+    int remainder, i=0;
+    
+    if(num==0)
+    {
+        result[i++] = '0';
+        result[i] = '\0';
+        return;
+    }
+    
+    while(num>0)
+    {
+        remainder=num % base;
+        
+        if(remainder<10)
+        {
+            result[i]=remainder + '0';
+        }
+        else
+        {
+            result[i]=remainder - 10 + 'A';
+        }
+        num = num / base;
+        i++;
+    }
+    
+    result[i] = '\0';
+}
+
+int main()
+{
+    int num, base;
+    char result[100];
+    
+    printf("Enter decimal number:");
+    scanf("%d", &num);
+    
+    printf("Enter base(2 to 16):");
+    scanf("%d", &base);
+    
+    if((base<2) || (base>16))
+    {
+        printf("Invalid number\n");
+        printf("Enter base(2 to 16)");
+        return 0;
+    }
+    
+    ConvertBase(num, base, result);
+    
+    printf("Converted number:");
+    
+    for(int i = strlen(result) - 1; i >= 0; i--)
+    {
+        printf("%c", result[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
