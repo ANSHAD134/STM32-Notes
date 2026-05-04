6d2f26a900af59944905d76af031ef6f18ef39e2
Anshad ck <anshadck1020@gmail.com>
2026-05-04 20:12:09 +0530

    Added Traffic_Light_Interrupt

--- AVR Assignment/Traffic_Light_Interrupt/Traffic_Light_Interrupt/main.c
+++ AVR Assignment/Traffic_Light_Interrupt/Traffic_Light_Interrupt/main.c
@@ -17,8 +17,6 @@ volatile uint8_t flag = 0;
 ISR(INT0_vect)
 {
 	flag = 1;
-	PORTB |= (1 << PORTB0);						// GREEN LED For High
-	_delay_ms(5000);							// 5 second delay
 }
 
 
@@ -38,19 +36,37 @@ int main(void)
     /* Replace with your application code */
     while (1) 
     {
-		if (flag == 0)
+		PORTB |= (1 << PORTB0);					// GREEN LED For High
+		_delay_ms(10000);						// 10 second delay
+		
+		if(flag)
 		{
-			PORTB |= (1 << PORTB0);				// GREEN LED For High
-			_delay_ms(10000);					// 10 second delay
-			PORTB0 &= ~(1 << PORTB0);			// GREEN LED for Low
+			PORTB |= (1 << PORTB0);				// Priority GREEN
+			_delay_ms(5000);					// 5 second delay
+			flag = 0;
+			continue;
+		}
 			
-			PORTB |= (1 << PORTB1);				// ORANGE LED For High
-			_delay_ms(3000);					// 3 second delay
-			PORTB &= ~(1 << PORTB1);			// ORANGE LED For Low
+		PORTB |= (1 << PORTB1);					// ORANGE LED For High
+		_delay_ms(3000);						// 3 second delay
+		
+		if(flag)
+		{
+			PORTB |= (1 << PORTB0);				// Priority GREEN
+			_delay_ms(5000);					// 5 second delay
+			flag = 0;
+			continue;
+		}
 			
-			PORTB |= (1 << PORTB2);				// RED LED For High
-			_delay_ms(10000);					// 10 second delay
-			PORTB &= ~(1 << PORTB2);			// RED LED For Low
+			
+		PORTB |= (1 << PORTB2);				// RED LED For High
+		_delay_ms(10000);					// 10 second delay
+		
+		if(flag)
+		{
+			PORTB |= (1 << PORTB0);				// Priority GREEN
+			_delay_ms(5000);					// 5 second delay
+			flag = 0;
 		}
     }
 }
