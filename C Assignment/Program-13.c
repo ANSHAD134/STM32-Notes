db36dd2ccc5e1d425aa44ce67778898f3df1b031
Anshad ck <anshadck1020@gmail.com>
2026-05-04 16:38:14 +0530

    Added Program-13

---
+++ C Assignment/Program-13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int len;
+    char str[100];
+    
+    printf("Enter a string:");
+    scanf("%s", str);
+    
+    len = strlen(str);
+    
+    for(int i=0; i<len/2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+    
+    printf("Reversed string: %s\n", str);
+    
+    return 0;
+}
