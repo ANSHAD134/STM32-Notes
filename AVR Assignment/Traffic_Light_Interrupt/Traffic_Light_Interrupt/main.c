9efdad66ba814cc001f7b5308f4f97a0a4353b67
Anshad ck <anshadck1020@gmail.com>
2026-05-04 20:05:26 +0530

    Added Traffic_Light_Interrupt

---
+++ AVR Assignment/Traffic_Light_Interrupt/Traffic_Light_Interrupt/main.c
@@ -0,0 +1,57 @@
+/*
+ * Traffic_Light_Interrupt.c
+ *
+ * Created: 04-05-2026 7.28.32 PM
+ * Author : HP
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+volatile uint8_t flag = 0;
+
+// Interrupt service routine for INT0
+ISR(INT0_vect)
+{
+	flag = 1;
+	PORTB |= (1 << PORTB0);						// GREEN LED For High
+	_delay_ms(5000);							// 5 second delay
+}
+
+
+int main(void)
+{
+	DDRB |= (1 << DDB0);						// Set Output for GREEN LED
+	DDRB |= (1 << DDB1);						// Set Output for ORANGE LED
+	DDRB |= (1 << DDB2);						// Set Output for RED LED
+	
+	DDRD &= ~(1 << DDD2);						// Set Input for Push-Button
+	PORTD |= (1 << PORTD2);						// Enable internal Pull-up
+	
+	EIMSK |= (1 << INT0);						// Enable INT0 at EIMSK Register
+	EICRA |= (1 << ISC01);						// Enable Falling edge detection in EICRA Register
+	sei();										// Enable global Interrupt at SREG
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		if (flag == 0)
+		{
+			PORTB |= (1 << PORTB0);				// GREEN LED For High
+			_delay_ms(10000);					// 10 second delay
+			PORTB0 &= ~(1 << PORTB0);			// GREEN LED for Low
+			
+			PORTB |= (1 << PORTB1);				// ORANGE LED For High
+			_delay_ms(3000);					// 3 second delay
+			PORTB &= ~(1 << PORTB1);			// ORANGE LED For Low
+			
+			PORTB |= (1 << PORTB2);				// RED LED For High
+			_delay_ms(10000);					// 10 second delay
+			PORTB &= ~(1 << PORTB2);			// RED LED For Low
+		}
+    }
+}
+
