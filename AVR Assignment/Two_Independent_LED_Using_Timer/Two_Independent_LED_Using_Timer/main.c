1166d52a99e3ab0bc51771873d521f524a73a8d9
Anshad ck <anshadck1020@gmail.com>
2026-05-05 20:20:50 +0530

    Added Two_Independent_LED_Using_Timer

--- AVR Assignment/Two_Independent_LED_Using_Timer/Two_Independent_LED_Using_Timer/main.c
+++ AVR Assignment/Two_Independent_LED_Using_Timer/Two_Independent_LED_Using_Timer/main.c
@@ -12,7 +12,7 @@ volatile uint16_t t2_count = 0;
 
 ISR(TIMER1_COMPA_vect)
 {
-	PORTB ^= (1 << PORTB1);							// Status LED is High
+	PORTB ^= (1 << PORTB1);								// Status LED is High
 }
 
 ISR(TIMER2_COMPA_vect)
@@ -28,24 +28,24 @@ ISR(TIMER2_COMPA_vect)
 
 int main(void)
 {
-	DDRB |= (1 << DDB1);							// Set output for Status LED
-	DDRB |= (1 << DDB2);							// Set output for Heartbeat LED
+	DDRB |= (1 << DDB1);								// Set output for Status LED
+	DDRB |= (1 << DDB2);								// Set output for Heartbeat LED
 	
 	// Timer1 (16-bit)
-	TCCR1B |= (1 << WGM12);							// CTC MODE
-	TCCR1B |= (1 << CS12) | (1 << CS10);
+	TCCR1B |= (1 << WGM12);								// CTC MODE
+	TCCR1B |= (1 << CS12) | (1 << CS10);				// Prescalar 1024
 	OCR1A = 31249;
 	
-	TIMSK1 |= (1 << OCIE1A);						// Output Compare Match A Interrupt
+	TIMSK1 |= (1 << OCIE1A);							// Enable Timer1 Compare Match A Interrupt
 	
 	// Timer 2 (8-bit)
-	TCCR2A |= (1 << WGM21);							// CTC MODE
-	TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
+	TCCR2A |= (1 << WGM21);								// CTC MODE
+	TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);	// Prescalar 1024
 	OCR2A = 124;
 	
-	TIMSK2 |= (1 << OCIE2A);
+	TIMSK2 |= (1 << OCIE2A);							// Enable Timer2 Compare Match A Interrupt
 	
-	sei();
+	sei();												// Enable Interrupts
 	
     /* Replace with your application code */
     while (1) 
