77a3f06b10e2fd9cb7efc954730cc2cf48e968ac
Anshad ck <anshadck1020@gmail.com>
2026-05-05 18:46:05 +0530

    Added Two_Independent_LED_Using_Timer

---
+++ AVR Assignment/Two_Independent_LED_Using_Timer/Two_Independent_LED_Using_Timer/main.c
@@ -0,0 +1,18 @@
+/*
+ * Two_Independent_LED_Using_Timer.c
+ *
+ * Created: 05-05-2026 6.41.57 PM
+ * Author : HP
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
