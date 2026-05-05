5f2a92267f55ef647d90b9f077c3e6d9db9995a8
Anshad ck <anshadck1020@gmail.com>
2026-05-05 20:17:57 +0530

    Added Two_Independent_LED_Using_Timer

--- AVR Assignment/Two_Independent_LED_Using_Timer/Two_Independent_LED_Using_Timer/main.c
+++ AVR Assignment/Two_Independent_LED_Using_Timer/Two_Independent_LED_Using_Timer/main.c
@@ -8,6 +8,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+volatile uint16_t t2_count = 0;
+
 ISR(TIMER1_COMPA_vect)
 {
 	PORTB ^= (1 << PORTB1);							// Status LED is High
@@ -15,7 +17,12 @@ ISR(TIMER1_COMPA_vect)
 
 ISR(TIMER2_COMPA_vect)
 {
-	PORTB ^= (1 << PORTB2);							// Heartbeat LED is High
+	t2_count++;
+	if(t2_count >= 63)
+	{
+		PORTB ^= (1 << PORTB2);							// Heartbeat LED is High
+		t2_count = 0;
+	}
 }
 
 
@@ -25,16 +32,20 @@ int main(void)
 	DDRB |= (1 << DDB2);							// Set output for Heartbeat LED
 	
 	// Timer1 (16-bit)
-	TCCR1A |= (1 << WGM01);							// CTC MODE
-	TCCR1A |= (1 << CS02) | (1 << CS00);
-	OCR0A = 31249;
+	TCCR1B |= (1 << WGM12);							// CTC MODE
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+	OCR1A = 31249;
 	
-	TIMSK0 |= (1 << OCIE0A);						// Output Compare Match A Interrupt
+	TIMSK1 |= (1 << OCIE1A);						// Output Compare Match A Interrupt
 	
 	// Timer 2 (8-bit)
-	TCCR2A |= (1 << WGM01);							// CTC MODE
-	OCR0A = 200;
+	TCCR2A |= (1 << WGM21);							// CTC MODE
+	TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
+	OCR2A = 124;
 	
+	TIMSK2 |= (1 << OCIE2A);
+	
+	sei();
 	
     /* Replace with your application code */
     while (1) 
