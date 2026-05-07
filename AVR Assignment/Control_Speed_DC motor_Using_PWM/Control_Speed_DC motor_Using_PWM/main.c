ff9ef5d6fc831b31c419f2c60e927ab506459170
Anshad ck <anshadck1020@gmail.com>
2026-05-07 19:20:41 +0530

    Added Control_Speed_DC motor_Using_PWM

---
+++ AVR Assignment/Control_Speed_DC motor_Using_PWM/Control_Speed_DC motor_Using_PWM/main.c
@@ -0,0 +1,18 @@
+/*
+ * Control_Speed_DC motor_Using_PWM.c
+ *
+ * Created: 07-05-2026 7.19.49 PM
+ * Author : HP
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
