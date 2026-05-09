78ab2e9a50c5384fe79a5201da7f60820ee992fe
Anshad ck <anshadck1020@gmail.com>
2026-05-09 19:57:18 +0530

    Added Automated_Cooling_System_Using_LM35

---
+++ AVR Assignment/Automated_Cooling_System_Using_LM35/Automated_Cooling_System_Using_LM35/main.c
@@ -0,0 +1,18 @@
+/*
+ * Automated_Cooling_System_Using_LM35.c
+ *
+ * Created: 09-05-2026 7.54.28 PM
+ * Author : HP
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
