e5c0eab8e67fcf12c988590bbc01bab26496cfd1
Anshad ck <anshadck1020@gmail.com>
2026-05-09 20:25:59 +0530

    Added Automated_Cooling_System_Using_LM35

--- AVR Assignment/Automated_Cooling_System_Using_LM35/Automated_Cooling_System_Using_LM35/main.c
+++ AVR Assignment/Automated_Cooling_System_Using_LM35/Automated_Cooling_System_Using_LM35/main.c
@@ -27,6 +27,54 @@ uint8_t ADC_Read(uint8_t channel)
 	return (ADCL | (ADCH << 8));											// Combine Result
 }
 
+void Motor_init(void)
+{
+	TCCR0A |= (1 << COM0A1);												// Non-inverting
+	
+	TCCR0B |= (1 << CS01) | (1 << CS00);									// Prescalar 64
+	TCCR0A |= (1 << WGM00) | (1 << WGM01);									// Fast PWM
+	OCR0A = 64;																// Start with 25% Motor speed
+}
+
+void Motor_Speed(void)
+{
+	uint8_t speed = 64;
+	static uint8_t state = 1;
+	
+	if(!(PIND&(1 << PIND2)))
+	{
+		_delay_ms(50);														// Debounce delay for Push-button
+		
+		state++;
+		if (state > 5)
+		state = 1;
+		
+		switch(state)
+		{
+			case 1:
+			speed = 64;														// 25% Motor speed
+			break;
+			
+			case 2:
+			speed = 128;													// 50% Motor speed
+			break;
+			
+			case 3:
+			speed = 192;													// 75% Motor speed
+			break;
+			
+			case 4:
+			speed = 255;													// 100% Motor speed
+			break;
+			
+			case 5:
+			speed = 0;														// Motor stop
+		}
+		OCR0A = speed;
+		while(!(PIND&(1 << PIND2)));										// Wait until button released
+	}
+}
+
 
 int main(void)
 {
@@ -37,6 +85,7 @@ int main(void)
 	
 	uint16_t adc_value;
 	ADC_init();	
+	Motor_init();
     /* Replace with your application code */
     while (1) 
     {
