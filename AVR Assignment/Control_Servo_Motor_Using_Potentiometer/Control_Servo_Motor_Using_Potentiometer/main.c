4659eb5326a0e496753859459d0cffc7cfb97aea
Anshad ck <anshadck1020@gmail.com>
2026-05-10 20:04:50 +0530

    Added Control_Servo_Motor_Using_Potentiometer

--- AVR Assignment/Control_Servo_Motor_Using_Potentiometer/Control_Servo_Motor_Using_Potentiometer/main.c
+++ AVR Assignment/Control_Servo_Motor_Using_Potentiometer/Control_Servo_Motor_Using_Potentiometer/main.c
@@ -13,26 +13,45 @@
 void ADC_init(void)
 {
 	ADMUX |= (1 << REFS0);													// Reference = AVCC (5V), input = ADC0
-	ADCSRA |= (1 << ADEN);													// Enable ADC
+	ADCSRA |= (1 << ADEN)| (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);		// Enable ADC, Prescaler = 128
+}
+
+// ADC Reading
+uint16_t ADC_Read(uint8_t channel)
+{
+	channel &= 0x07;														// Limit to 0-7
+	ADMUX = (ADMUX & 0xF8) | channel;										// Select channel
+	ADCSRA |= (1 << ADSC);													// Start conversion
+	while(ADCSRA & (1 << ADSC));											// Wait for conversion complete
+	return (ADCL | (ADCH << 8));											// Combine Result
 }
 
 // Timer1 (16-bit)
 void Timer1_Config(void)
 {
-	TIMSK1 |= (1 << OCIE1A);												// Enable Timer1 
+	DDRB |=(1 << DDB1);														// Output for Servo motor
 	
-	TCCR0B |= (1 << CS01) | (1 << CS00);									// Prescalar 64
-	TCCR0A |= (1 << WGM00) | (1 << WGM01);									// Fast PWM
-	OCR0A = 64;
+	TCCR1B |= (1 << CS11) | (1 << CS10);									// Prescalar 64
+	TCCR1A |= (1 << WGM10) | (1 << WGM11);									// Fast PWM
+	OCR1A = 20000;
 }
 
 int main(void)
 {
-	DDRB |=(1 << DDB1);														// Output for Servo motor
+	uint16_t adc_value;
+	uint16_t servo_position;
+	
+	ADC_init();
+	Timer1_Config();
 	
     /* Replace with your application code */
     while (1) 
     {
+		adc_value = ADC_Read(0);											// Read from analog channel 0
+		
+		servo_position = 1000 + ((adc_value * 1000UL) / 1023);				// ADC (0-1023) to Servo pulse (1000-2000)
+		
+		OCR1A = servo_position;
     }
 }
 
