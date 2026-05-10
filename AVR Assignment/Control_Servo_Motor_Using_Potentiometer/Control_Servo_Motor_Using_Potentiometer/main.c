f46ec02788a582af03dad477f671e3f74d492045
Anshad ck <anshadck1020@gmail.com>
2026-05-10 19:40:59 +0530

    Added Automated_Cooling_System_Using_LM35

---
+++ AVR Assignment/Control_Servo_Motor_Using_Potentiometer/Control_Servo_Motor_Using_Potentiometer/main.c
@@ -0,0 +1,18 @@
+/*
+ * Control_Servo_Motor_Using_Potentiometer.c
+ *
+ * Created: 10-05-2026 7.37.05 PM
+ * Author : HP
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
