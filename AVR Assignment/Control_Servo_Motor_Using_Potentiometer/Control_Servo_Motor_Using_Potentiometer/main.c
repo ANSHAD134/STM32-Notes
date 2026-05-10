101b0448e00eea8b1099164b396dde7a791c6529
Anshad ck <anshadck1020@gmail.com>
2026-05-10 19:41:40 +0530

    Added Control_Servo_Motor_Using_Potentiometer

--- AVR Assignment/Control_Servo_Motor_Using_Potentiometer/Control_Servo_Motor_Using_Potentiometer/main.c
+++ AVR Assignment/Control_Servo_Motor_Using_Potentiometer/Control_Servo_Motor_Using_Potentiometer/main.c
@@ -5,11 +5,18 @@
  * Author : HP
  */ 
 
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
+#include <util/delay.h>
+
+
 
 
 int main(void)
 {
+	DDRB |=(1 << DDB1);																		// Output for Servo motor
+	
     /* Replace with your application code */
     while (1) 
     {
