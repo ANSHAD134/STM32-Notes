32e4873a56e89f2c8bd4ff1f2ce2243d32ec2236
Anshad ck <anshadck1020@gmail.com>
2026-05-13 20:03:14 +0530

    Added UART_LED_Control

---
+++ AVR Assignment/UART_LED_Control/UART_LED_Control/main.c
@@ -0,0 +1,21 @@
+/*
+ * UART_LED_Control.c
+ *
+ * Created: 13-05-2026 7.46.49 PM
+ * Author : HP
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
