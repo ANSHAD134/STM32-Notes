f715dc749ad00d4b9ec5a24b5f414e986773e852
Anshad ck <anshadck1020@gmail.com>
2026-05-13 20:28:40 +0530

    Added UART_LED_Control

--- AVR Assignment/UART_LED_Control/UART_LED_Control/main.c
+++ AVR Assignment/UART_LED_Control/UART_LED_Control/main.c
@@ -15,10 +15,68 @@
 
 int main(void)
 {
-	char[]
+	char buffer[50];
+	uint8_t index = 0;
+	char received;
+	uint8_t brightness;
+	
+	UART_Init();
+	PWM_init();
+	
+	UART_TxString("UART LED Control Ready\n");
+	
     /* Replace with your application code */
     while (1) 
     {
-    }
+		received = UART_RxChar();
+
+		if(received == '\r' || received == '\n')
+		{
+			buffer[index] = '\0';
+
+			if(strcmp(buffer, "ON") == 0)						// ON Command
+			{
+				LED_Brightness(100);
+
+				UART_TxString("LED is now ON\r\n");
+			}
+
+			else if(strcmp(buffer, "OFF") == 0)					// OFF Command
+			{
+				LED_Brightness(0);
+
+				UART_TxString("LED is now OFF\r\n");
+			}
+			else
+			{
+				brightness = atoi(buffer);
+
+				if(brightness <= 100)							// Brightness Control
+				{
+					char msg[40];
+
+					LED_Brightness(brightness);
+
+					sprintf(msg, "Brightness set to %d%%\r\n", brightness);
+
+					UART_TxString(msg);
+				}
+				else
+				{
+					UART_TxString("Invalid Brightness Value\r\n");
+				}
+			}
+
+			index = 0;
+		}
+
+		else
+		{
+			if(index < sizeof(buffer)-1)
+			{
+				buffer[index++] = received;
+			}
+		}
+	}
 }
 
