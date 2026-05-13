5366e09861752d03743d55774f75f8a3703f66bc
Anshad ck <anshadck1020@gmail.com>
2026-05-13 20:21:26 +0530

    Added UART_LED_Control

--- AVR Assignment/UART_LED_Control/UART_LED_Control/pwm.c
+++ AVR Assignment/UART_LED_Control/UART_LED_Control/pwm.c
@@ -4,3 +4,24 @@
  * Created: 13-05-2026 8.04.01 PM
  *  Author: HP
  */ 
+
+#include "pwm.h"
+
+void PWM_init(void)
+{
+	DDRD |= (1 << DDD6);											// Set PD6 as output
+	
+	TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);			// Fast PWM, Non-inverting
+	TCCR0B |= (1 << CS01) | (1 << CS00);							// Prescaler 64
+	
+	OCR0A = 0;
+}
+
+void LED_Brightness(uint8_t brightness)
+{
+	uint16_t pwm_value;
+	
+	OCR0A = (brightness * 255UL) / 100;								// Convert 0-255
+	
+	OCR0A = pwm_value;
+}
