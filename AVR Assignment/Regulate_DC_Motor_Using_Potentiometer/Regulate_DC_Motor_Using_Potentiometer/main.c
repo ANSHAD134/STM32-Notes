09482fd20095eb34819afc1bc564e58cc2843fed
Anshad ck <anshadck1020@gmail.com>
2026-05-16 13:24:45 +0530

    Added Regulate_DC_Motor_Using_Potentiometer

--- AVR Assignment/Regulate_DC_Motor_Using_Potentiometer/Regulate_DC_Motor_Using_Potentiometer/main.c
+++ AVR Assignment/Regulate_DC_Motor_Using_Potentiometer/Regulate_DC_Motor_Using_Potentiometer/main.c
@@ -5,14 +5,28 @@
  * Author : HP
  */ 
 
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
+#include "adc.h"
+#include "pwm.h"
 
 
 int main(void)
 {
+	uint16_t adc_value;
+	uint8_t pwm_value;
+	
+	ADC_init();											// Initialize ADC
+	PWM_Init();											// Initialize PWM
     /* Replace with your application code */
     while (1) 
     {
+		adc_value = ADC_Read(0);						// Read potentiometer from ADC0
+		
+		pwm_value = adc_value / 4;						// Convert 10-bit ADC to 8-bit PWM
+		
+		Motor_Speed(pwm_value);							// Control motor speed
     }
 }
 
