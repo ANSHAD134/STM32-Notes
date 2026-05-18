742ed604619ed0a830ae5488cf536a9dcfeb28c1
Anshad ck <anshadck1020@gmail.com>
2026-05-18 20:17:05 +0530

    Added LED_Train_animation

--- AVR Assignment/LED_Train_animation/LED_Train_animation/main.c
+++ AVR Assignment/LED_Train_animation/LED_Train_animation/main.c
@@ -19,27 +19,50 @@ int main(void)
 	DDRB |= (1 << DDB3);					// Set PORT3 as OUTPUT
 	DDRB |= (1 << DDB4);					// Set PORT4 as OUTPUT
 	DDRB |= (1 << DDB5);					// Set PORT5 as OUTPUT
-	DDRB |= (1 << DDB6);					// Set PORT6 as OUTPUT
-	DDRB |= (1 << DDB7);					// Set PORT7 as OUTPUT
+	DDRB |= (1 << DDD6);					// Set PORT6 as OUTPUT
+	DDRB |= (1 << DDD7);					// Set PORT7 as OUTPUT
 	
     /* Replace with your application code */
     while (1) 
     {
-		for(uint8_t i=0; i<8; i++)
-		{
-			PORTB &= ~(1 << PORTB0);			// Clear 1st LED
-			PORTB &= ~(1 << PORTB1);			// Clear 2nd LED
-			PORTB &= ~(1 << PORTB2);			// Clear 3rd LED
-			PORTB &= ~(1 << PORTB3);			// Clear 4th LED
-			PORTB &= ~(1 << PORTB4);			// Clear 5th LED
-			PORTB &= ~(1 << PORTB5);			// Clear 6th LED
-			PORTB &= ~(1 << PORTB6);			// Clear 7th LED
-			PORTB &= ~(1 << PORTB7);			// Clear 8th LED
-			
-			PORTB |= (1 << i);					// Turns ON current LED
-			
-			_delay_ms(200);						// 200ms delay
-		}
+		PORTB &= ~(1 << PORTB1);
+		PORTB |=  (1 << PORTB0);
+		_delay_ms(200);
+		PORTB &= ~(1 << PORTB0);
+		
+		PORTB &= ~(1 << PORTB2);
+		PORTB |=  (1 << PORTB1);
+		_delay_ms(200);
+		PORTB &= ~(1 << PORTB1);
+		
+		PORTB &= ~(1 << PORTB3);
+		PORTB |=  (1 << PORTB2);
+		_delay_ms(200);
+		PORTB &= ~(1 << PORTB2);
+		
+		PORTB &= ~(1 << PORTB4);
+		PORTB |=  (1 << PORTB3);
+		_delay_ms(200);
+		PORTB &= ~(1 << PORTB3);
+		
+		PORTB &= ~(1 << PORTB5);
+		PORTB |=  (1 << PORTB4);
+		_delay_ms(200);
+		PORTB &= ~(1 << PORTB4);
+		
+		PORTD &= ~(1 << PORTD6);
+		PORTB |=  (1 << PORTB5);
+		_delay_ms(200);
+		PORTB &= ~(1 << PORTB5);
+
+		PORTD &= ~(1 << PORTD7);
+		PORTD |=  (1 << PORTD6);
+		_delay_ms(200);
+		PORTD &= ~(1 << PORTD6);
+		
+		PORTD |=  (1 << PORTD7);
+		_delay_ms(200);
+		PORTD &= ~(1 << PORTD7);
     }
 }
 
