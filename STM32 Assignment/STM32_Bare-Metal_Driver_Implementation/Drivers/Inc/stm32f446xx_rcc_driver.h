d2dfb8b396d68c530cade2f779ee20b19df5d8c7
Anshad ck <anshadck1020@gmail.com>
2026-05-23 20:44:16 +0530

    Edited STM32_Bare-Metal_Driver_Implementation

---
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_rcc_driver.h
@@ -0,0 +1,28 @@
+/*
+ * stm32f446xx_rcc_driver.h
+ *
+ *  Created on: 23-May-2026
+ *      Author: HP
+ */
+
+#ifndef INC_STM32F446XX_RCC_DRIVER_H_
+#define INC_STM32F446XX_RCC_DRIVER_H_
+
+#include "stm32f446xx.h"
+
+/*
+ * Returns the APB1 peripheral clock value
+ */
+uint32_t RCC_GetPCLK1Value(void);
+
+/*
+ * Returns the APB2 peripheral clock value
+ */
+uint32_t RCC_GetPCLK2Value(void);
+
+/*
+ * Returns the PLL output clock value
+ */
+uint32_t RCC_GetPLLOutputClock(void);
+
+#endif /* INC_STM32F446XX_RCC_DRIVER_H_ */
