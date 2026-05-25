36d3d3899306343a5cacbe0b9cd39c54fa5a7145
Anshad ck <anshadck1020@gmail.com>
2026-05-25 20:21:11 +0530

    Edited STM32_Bare-Metal_Driver_Implementation

---
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Drivers/Src/stm32f446xx_usart_driver.d
@@ -0,0 +1,8 @@
+Drivers/Src/stm32f446xx_usart_driver.o: \
+ ../Drivers/Src/stm32f446xx_usart_driver.c \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_usart_driver.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_rcc_driver.h
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_usart_driver.h:
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h:
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_rcc_driver.h:
