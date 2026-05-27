fb65a1224ed645fba3921afd41f3530a35e561d6
Anshad ck <anshadck1020@gmail.com>
2026-05-27 21:59:09 +0530

    Added main_usart

---
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Src/main_uart.d
@@ -0,0 +1,9 @@
+Src/main_uart.o: ../Src/main_uart.c \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_gpio_driver.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_usart_driver.h
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h:
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_gpio_driver.h:
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h:
+C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_usart_driver.h:
