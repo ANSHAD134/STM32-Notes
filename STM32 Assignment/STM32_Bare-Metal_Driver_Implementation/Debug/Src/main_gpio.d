a020a00c033450825a962a15a4d94600294435a8
Anshad ck <anshadck1020@gmail.com>
2026-05-27 20:19:03 +0530

    Added main_usart

--- STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Src/main_gpio.d
+++
@@ -1,5 +0,0 @@
-Src/main_gpio.o: ../Src/main_gpio.c \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_gpio_driver.h \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_gpio_driver.h:
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h:
