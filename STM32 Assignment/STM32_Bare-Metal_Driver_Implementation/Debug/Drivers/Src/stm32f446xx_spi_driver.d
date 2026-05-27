98c4e7d3ecb39da5791f220e5385c718afb442c8
Anshad ck <anshadck1020@gmail.com>
2026-05-27 20:37:10 +0530

    Added main_usart

--- STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Drivers/Src/stm32f446xx_spi_driver.d
+++
@@ -1,6 +0,0 @@
-Drivers/Src/stm32f446xx_spi_driver.o: \
- ../Drivers/Src/stm32f446xx_spi_driver.c \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_spi_driver.h \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_spi_driver.h:
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h:
--- STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Drivers/Src/stm32f446xx_usart_driver.d
+++
@@ -1,8 +0,0 @@
-Drivers/Src/stm32f446xx_usart_driver.o: \
- ../Drivers/Src/stm32f446xx_usart_driver.c \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_usart_driver.h \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h \
- C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_rcc_driver.h
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_usart_driver.h:
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx.h:
-C:/Users/HP/Documents/C\ Programming/STM32/STM32\ Assignment/STM32_Bare-Metal_Driver_Implementation/Drivers/Inc/stm32f446xx_rcc_driver.h:
