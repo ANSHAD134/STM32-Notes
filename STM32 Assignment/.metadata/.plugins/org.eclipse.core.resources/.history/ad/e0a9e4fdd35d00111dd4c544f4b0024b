89792c191ed2e9d61e2ed9bea34037e20d8d3d20
Anshad ck <anshadck1020@gmail.com>
2026-06-01 21:37:54 +0530

    Added main_i2c

---
+++ STM32 Assignment/.metadata/.plugins/org.eclipse.core.resources/.history/ad/e0a9e4fdd35d00111dd4c544f4b0024b
@@ -0,0 +1,11 @@
+/*
+ * bmp280.c
+ *
+ *  Created on: 01-Jun-2026
+ *      Author: HP
+ */
+
+#include "stm32f446xx.h"
+#include "stm32f446xx_i2c_driver.h"
+
+
--- STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Src/bmp280.c
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Src/bmp280.c
@@ -7,5 +7,18 @@
 
 #include "stm32f446xx.h"
 #include "stm32f446xx_i2c_driver.h"
+#include "bmp280.h"
 
+// Calibration variables from BMP280
+
+uint16_t dig_T1;
+int16_t dig_T2, dig_T3;
+int32_t t_fine;
+
+void BMP280_Init(void)
+{
+	for(uint32_t i = 0; i < 500000; i++);						// Wait sensor to power up
+
+
+}
 
