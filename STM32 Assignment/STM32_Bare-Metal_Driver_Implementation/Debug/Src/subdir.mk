05ce4c4a12336dbf72b909eea292d5e6d53a8508
Anshad ck <anshadck1020@gmail.com>
2026-06-01 20:58:12 +0530

    Added main_i2c

--- STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Src/subdir.mk
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/main_uart.c \
+../Src/main_i2c.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/main_uart.o \
+./Src/main_i2c.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/main_uart.d \
+./Src/main_i2c.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main_uart.cyclo ./Src/main_uart.d ./Src/main_uart.o ./Src/main_uart.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/main_i2c.cyclo ./Src/main_i2c.d ./Src/main_i2c.o ./Src/main_i2c.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
