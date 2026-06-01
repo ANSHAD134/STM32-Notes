af16f7b9687d79804dc10087e67d65fbe8f16445
Anshad ck <anshadck1020@gmail.com>
2026-06-01 21:02:42 +0530

    Added main_i2c

---
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Inc/bmp280.h
@@ -0,0 +1,13 @@
+/*
+ * bmp280.h
+ *
+ *  Created on: 01-Jun-2026
+ *      Author: HP
+ */
+
+#ifndef BMP280_H_
+#define BMP280_H_
+
+#define BMP280_ADDR			0x76				// BMP280 Address
+
+#endif /* BMP280_H_ */
