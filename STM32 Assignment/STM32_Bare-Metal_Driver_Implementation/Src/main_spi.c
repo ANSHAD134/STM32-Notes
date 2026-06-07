d198059804e5591b78b9a9a88719c44ec51a368b
Anshad ck <anshadck1020@gmail.com>
2026-06-07 19:42:38 +0530

    Added Main_spi.c

--- STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Src/main_spi.c
+++ STM32 Assignment/STM32_Bare-Metal_Driver_Implementation/Src/main_spi.c
@@ -4,5 +4,21 @@
  *  Created on: 07-Jun-2026
  *      Author: HP
  */
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
+#include "stm32f446xx.h"
+#include "stm32f446xx_gpio_driver.h"
+#include "stm32f446xx_spi_driver.h"
+
+int main(void)
+{
+
+
+	while(1)
+	{
+
+	}
+}
 
