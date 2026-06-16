b48e24e11bec9290e835cce44132387ce938654b
Anshad ck <anshadck1020@gmail.com>
2026-06-16 21:36:19 +0530

    Added RTOS_1

---
+++ RTOS_Workspace/RTOS_1/Debug/ThirdParty/FreeRtos/queue.d
@@ -0,0 +1,23 @@
+ThirdParty/FreeRtos/queue.o: ../ThirdParty/FreeRtos/queue.c \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/FreeRTOS.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/FreeRTOSConfig.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/projdefs.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/portable.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/deprecated_definitions.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/mpu_wrappers.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/task.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/list.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/queue.h \
+ C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/task.h
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/FreeRTOS.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/FreeRTOSConfig.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/projdefs.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/portable.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/deprecated_definitions.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/mpu_wrappers.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/task.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/list.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/queue.h:
+C:/Users/HP/Documents/C\ Programming/STM32/RTOS_Workspace/RTOS_1/ThirdParty/FreeRtos/include/task.h:
